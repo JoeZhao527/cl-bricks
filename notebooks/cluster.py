16a31b7c916aad3b856f3217ea803311c9081cd0
haokai-jojo <haokai@biomap.com>
2025-01-18 16:13:04 +0800

    K-means++ clustering

--- notebooks/cluster.py
+++ notebooks/cluster.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
 # Load the data
-data = torch.concat(torch.load("./prediction.pt"))
+data = torch.concat(torch.load("./prediction.pt"))[:10000]
 
 # Step 1: Normalize the data
 scaler = StandardScaler()
@@ -14,7 +14,7 @@ data_normalized = scaler.fit_transform(data.numpy())
 
 # KMeans++ clustering (KMeans++ is the default initialization method in sklearn's KMeans)
 # 6 clusters for 6 super parent classes
-kmeans = KMeans(init='k-means++', n_clusters=6, n_jobs=12, verbose=1)  
+kmeans = KMeans(init='k-means++', n_clusters=6, verbose=1)  
 kmeans.fit(data_normalized)
 
 # Get the cluster labels for each sample
