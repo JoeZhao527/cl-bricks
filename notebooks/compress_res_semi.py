3e4541c29e8f7153abd60497e30f453690e821ad
haokai-jojo <haokai@biomap.com>
2025-01-24 20:53:27 +0800

    balanced semi

---
+++ notebooks/compress_res_semi.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+dev = pd.read_csv("0124_semi_supervise_balanced.csv")
+arr = dev.drop(columns=["filename"]).values
+np.save("0124_semi_supervise_balanced.npy", np.stack(np.where(arr ==1)))
