13e6ab93c7ef0ef90cefa14330ff3000119ffc96
haokai-jojo <haokai@biomap.com>
2025-01-28 13:36:41 +0800

    clean up

--- bin/data_prep.sh
+++
@@ -1,4 +0,0 @@
-python data_prep/preprocess_parallel.py --split_num=1 --num_workers=8 --output_dir=./processed/feat_split1
-python data_prep/preprocess_parallel.py --split_num=2 --num_workers=8 --output_dir=./processed/feat_split2
-python data_prep/preprocess_parallel.py --split_num=3 --num_workers=8 --output_dir=./processed/feat_split3
-python data_prep/preprocess_parallel.py --split_num=4 --num_workers=8 --output_dir=./processed/feat_split4
--- main.py
+++ main.py
@@ -1,18 +1,37 @@
+# Run function for one model, multiple train set, one test set
 from ensemble.pipeline.base_random_forest import run as base_model_run
+
+# Run function for multiple models, multiple train set, multiple test set
 from ensemble.pipeline.base_multi_model import run as ensemble_run
+
+# Run function for multiple models stacked on previous output, multiple train set, multiple test set
 from ensemble.pipeline.stacking_multi_model import run as stack_ensemble_run
-from ensemble.config.pipeline import base_rf, base_lgb, base_xgboost, base_ensemble, stacking_ensemble, weighted_rf
+
+# Configurations. Each one is a dict of configurations
+from ensemble.config.pipeline import (
+    base_rf, base_lgb, base_xgboost, weighted_rf,
+    base_ensemble, stacking_ensemble
+)
+
 from copy import deepcopy
 
 if __name__ == '__main__':
-    cfg = deepcopy(weighted_rf)
-    # cfg["data"]["n_train_sets"] = 2
-    # cfg["data"]["n_splits"] = 2
+    """
+    Before running, make sure update the paths in ensemble.config.paths.PATHS.
+    All used data path will be defined there. 
 
-    # cfg["model"]["model_params"]["n_estimators"] = 2
+    Current version does not save model for memory. Running complete pipeline will 
+    probably stack on the local server.
+    """
+    # Prepare configurations
+    cfg = deepcopy(base_ensemble)
 
-    # for k, v in cfg["model"].items():
-    #     cfg["model"][k]["model_params"]["n_estimators"] = 2
+    ### COMMENT OUT THE FOLLOWING FOR COMPLETE RUN
+    cfg["data"]["n_train_sets"] = 2
+    cfg["data"]["n_splits"] = 2
 
-    # stack_ensemble_run(cfg)
-    base_model_run(cfg)
+    for k, v in cfg["model"].items():
+        cfg["model"][k]["model_params"]["n_estimators"] = 2
+
+    # Start runninng
+    ensemble_run(cfg)
