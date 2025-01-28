0e3204caded9ac4b4bedafcca9ce1ed523d88973
haokai-jojo <haokai@biomap.com>
2025-01-28 11:27:25 +0800

    stacking lets go

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from ensemble.config.pipeline import base_rf, base_lgb, base_xgboost, base_ensem
 from copy import deepcopy
 
 if __name__ == '__main__':
-    cfg = deepcopy(base_rf)
+    cfg = deepcopy(stacking_ensemble)
     cfg["data"]["n_train_sets"] = 2
     cfg["data"]["n_splits"] = 2
 
