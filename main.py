06877dffb24f36da9383b042b03cdef83e4a7073
haokai-jojo <haokai@biomap.com>
2025-01-28 11:28:21 +0800

    stacking lets go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from ensemble.pipeline.base_random_forest import run as base_model_run
 from ensemble.pipeline.base_multi_model import run as ensemble_run
+from ensemble.pipeline.stacking_multi_model import run as stack_ensemble_run
 from ensemble.config.pipeline import base_rf, base_lgb, base_xgboost, base_ensemble, stacking_ensemble
 from copy import deepcopy
 
@@ -8,9 +9,9 @@ if __name__ == '__main__':
     cfg["data"]["n_train_sets"] = 2
     cfg["data"]["n_splits"] = 2
 
-    cfg["model"]["model_params"]["n_estimators"] = 2
+    # cfg["model"]["model_params"]["n_estimators"] = 2
 
-    # for k, v in cfg["model"].items():
-    #     cfg["model"][k]["model_params"]["n_estimators"] = 2
+    for k, v in cfg["model"].items():
+        cfg["model"][k]["model_params"]["n_estimators"] = 2
 
-    ensemble_run(cfg)
+    stack_ensemble_run(cfg)
